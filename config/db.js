edaab1e0799690e552910e561211fc2d60d45c95
bw007 <mr.abdulaziz00791@gmail.com>
2025-04-28 16:54:04 +0500

    fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 // Create a new pool instance
 const pool = new Pool({
   user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD + '',
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT
