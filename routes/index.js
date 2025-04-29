c7e1639d77bfb6c80755c192eeb81038f729a0c3
bw007 <mr.abdulaziz00791@gmail.com>
2025-04-29 17:30:26 +0500

    Update home route to use the correct home module

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.use("/", require("./main"))
+router.use("/", require("./home"))
 router.use("/patients", require("./patients"));
 
 module.exports = router;
