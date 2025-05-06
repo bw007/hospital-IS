5eb186bed003d9e5956551a2893fa2c8f595c2b3
bw007 <mr.abdulaziz00791@gmail.com>
2025-05-06 10:22:38 +0500

    Update

---
+++ controllers/doctorController.js
@@ -0,0 +1,15 @@
+const Doctor = require('../models/Doctor');
+
+// Get all doctors
+const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find();
+    res.status(200).json(doctors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllDoctors
+};
