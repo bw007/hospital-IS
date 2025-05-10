97c00a40691047ba1c9802965946667c81dd205b
bw007 <abdulaziz00791@gmail.com>
2025-05-10 19:06:23 +0500

    Add doctor count to home page data rendering

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,11 +2,13 @@ const pool = require("../config/db");
 
 const getHomePage = async (req, res) => {
   try {
+    const doctors = await pool.query("SELECT COUNT(*) FROM doctors");
     const patients = await pool.query("SELECT COUNT(*) FROM patients");
     const appointments = await pool.query("SELECT COUNT(*) FROM appointments");
 
     res.render("pages/index", {
       title: "Home",
+      doctorsTotal: parseInt(doctors.rows[0].count),
       patientsTotal: parseInt(patients.rows[0].count),
       appointmentsTotal: parseInt(appointments.rows[0].count),
     });
